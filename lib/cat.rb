43aeeeedf4c8ccf2f6080b7bc63589bc36254cc5
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-01-23 16:28:44 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,13 @@
 class Cat
-  # code goes here
+  attr_accessor :name, :owner 
+  
+  def initialize(name,owner)
+    @name= name 
+    @owner=owner 
+  end 
+  
+  def owner
+    @name=name
+    
+  end 
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,30 @@
 class Owner
-  # code goes here
-end
+  
+  attr_reader :name , :species 
+  @@all = []
+  @@owner_count =0 
+
+  def initialize (name, species="human")
+    @name= name 
+    @species= species 
+    @@all << self 
+    @@owner_count +=1 
+  end
+  
+  def say_species 
+   "I am a #{species}."
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.count 
+    @@owner_count
+  end 
+  
+  def self.reset_all 
+    @@all.clear 
+  end 
+  
+end 
